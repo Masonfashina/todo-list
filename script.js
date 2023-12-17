fd3ee6435c52fdc0a3ecb82a7eac1c967eb156b6
Mason <Masonfashina@icloud.com>
2023-12-17 14:20:21 +0100

    stage 2 done

--- script.js
+++ script.js
@@ -4,13 +4,43 @@ const getTodos = () => {
   fetch(apiUrl + "?_limit=6")
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((todo) => {
-        const div = document.createElement("div");
-        div.appendChild(document.createTextNode(todo.title));
-
-        document.getElementById("todo-list").appendChild(div)
-      });
+      data.forEach((todo) => addTodoToDom(todo));
     });
 };
+const addTodoToDom = (todo) => {
+  const div = document.createElement("div");
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute("data-id", todo.id);
 
-getTodos();
+  if (todo.completed) {
+    div.classList.add("done");
+  }
+
+  document.getElementById("todo-list").appendChild(div);
+};
+
+const createTodo = (e) => {
+  e.preventDefault();
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false,
+  };
+
+  fetch(apiUrl, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => addTodoToDom(data));
+};
+
+const init = () => {
+  // Get todos from API and populate the page
+  document.addEventListener("DOMContentLoaded", getTodos);
+  document.querySelector("#todo-form").addEventListener("submit", createTodo);
+};
+
+init();
